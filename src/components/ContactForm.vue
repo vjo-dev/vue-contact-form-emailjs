c4c6fbab6dbef45fe5200862b6027014f8a88868
Vincent Journot <journot.vincent@gmail.com>
2023-12-14 12:01:54 +0100

    add honeypot safety and email verification

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,18 +1,40 @@
 <script setup>
 import emailjs from "emailjs-com";
-import { ref, computed } from "vue";
+import { ref } from "vue";
 
-const name = ref("");
+// fields
+const namehjkl = ref("");
+const emailhjkl = ref("");
+const messagehjkl = ref("");
+const isEmailInvalid = ref(false);
+// otherunusedfield
+const name = ref("Steve");
 const email = ref("");
 const message = ref("");
 
-// disable submit button is form is incomplete
-const buttonStatus = computed(() => {
-  return name.value == "" || email.value == "" || message.value == "";
-});
+// function to reset field error
+function resetInvalid() {
+  if (isEmailInvalid.value) {
+    isEmailInvalid.value = !isEmailInvalid.value;
+  }
+}
 
 // action to submit the form
 function sendEmail() {
+  // prevent with otherunusedfield
+  if (name.value != "Steve" || email.value != "" || message.value != "") {
+    console.log("bot detected");
+    return;
+  }
+
+  // email string validation
+  if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailhjkl.value)) {
+    console.log("invalid email");
+    isEmailInvalid.value = true;
+    return;
+  }
+
+  // sent form
   try {
     console.log("form submitted.");
   } catch (err) {
@@ -23,8 +45,14 @@ function sendEmail() {
     }
   }
 
-  // Reset form field
-  name.value = "";
+  // Reset fields
+  namehjkl.value = "";
+  emailhjkl.value = "";
+  messagehjkl.value = "";
+  isEmailInvalid.value = false;
+
+  // Reset otherunusedfield
+  name.value = "Steve";
   email.value = "";
   message.value = "";
 }
@@ -33,36 +61,84 @@ function sendEmail() {
 <template>
   <div class="container">
     <form v-on:submit.prevent="sendEmail">
+      <!-- fields -->
       <label>Name</label>
-      <input type="text" v-model="name" name="name" placeholder="Your Name" />
+      <input
+        type="text"
+        v-model="namehjkl"
+        name="namehjkl"
+        placeholder="Your Name"
+        maxlength="100"
+        required
+      />
       <label>Email</label>
       <input
+        :class="{ invalid: isEmailInvalid }"
+        @click="resetInvalid"
+        type="email"
+        v-model="emailhjkl"
+        name="emailhjkl"
+        placeholder="Your Email"
+        required
+      />
+      <label>Message</label>
+      <textarea
+        name="messagehjkl"
+        v-model="messagehjkl"
+        cols="30"
+        rows="5"
+        placeholder="Message"
+        required
+      >
+      </textarea>
+      <input type="submit" value="Send" />
+
+      <!-- otherunusedfield -->
+      <label class="fields">Name</label>
+      <input
+        class="fields"
+        type="text"
+        v-model="name"
+        name="name"
+        placeholder="Your Name"
+        autocomplete="off"
+        required
+      />
+      <label class="fields">Email</label>
+      <input
+        class="fields"
         type="email"
         v-model="email"
         name="email"
         placeholder="Your Email"
+        autocomplete="off"
       />
-      <label>Message</label>
+      <label class="fields">Message</label>
       <textarea
+        class="fields"
         name="message"
         v-model="message"
         cols="30"
         rows="5"
         placeholder="Message"
+        autocomplete="off"
       >
       </textarea>
-      <input
-        class="test"
-        :class="buttonStatus ? 'disable' : 'enable'"
-        :disabled="buttonStatus"
-        type="submit"
-        value="Send"
-      />
     </form>
   </div>
 </template>
 
 <style>
+.fields {
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 0;
+  width: 0;
+  z-index: -1;
+}
+
 .container {
   display: block;
   margin: auto;
@@ -89,6 +165,7 @@ textarea {
   margin-top: 6px;
   margin-bottom: 16px;
   resize: vertical;
+  font-size: 1em;
 }
 
 input[type="submit"]:disabled {
@@ -109,5 +186,9 @@ input[type="submit"] {
   &:not(.disable):hover {
     background-color: #41b883;
   }
+}
+
+.invalid {
+  border: solid 4px red;
 }
 </style>
